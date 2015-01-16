974993b10abe4003a996af83b2b08bec25103912
Jonathan Balls <j.balls@btopenworld.com>
2015-01-16 11:02:37 +0000

    updated package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='cabot-alert-twilio',
-      version='1.0',
+      version='1.1',
       description='A twilio alert plugin for Cabot by Arachnys',
       author='Arachnys',
       author_email='info@arachnys.org',
@@ -11,5 +11,5 @@ setup(name='cabot-alert-twilio',
       packages=[
       	'cabot_alert_twilio'
       	],
-      download_url= 'https://github.com/bonniejools/cabot-alert-twilio/tarball/0.1'
+      download_url= 'https://github.com/bonniejools/cabot-alert-twilio/tarball/0.11'
      )

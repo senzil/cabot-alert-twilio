57492b1c184060a03db78d5953f0b8c82b624dfd
David Buxton <david@arachnys.com>
2015-02-09 19:05:17 +0000

    Fix bad urls file

--- cabot_alert_twilio/urls.py
+++ cabot_alert_twilio/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 from .views import twiml_callback
 
 urlpatterns = patterns('',
-    url(r'^result/(?P<service_id>\d+)/twiml_callback/', twiml_callback, verbose_name="twiml_callback"),
+    url(r'^result/(?P<service_id>\d+)/twiml_callback/', twiml_callback, name="twiml_callback"),
 )
--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(name='cabot-alert-twilio',
-      version='1.1.2',
+      version='1.1.3',
       description='A twilio alert plugin for Cabot by Arachnys',
       author='Arachnys',
       author_email='info@arachnys.org',
       url='http://cabotapp.com',
       packages=find_packages(),
-      download_url= 'https://github.com/bonniejools/cabot-alert-twilio/tarball/1.1.2'
+      download_url= 'https://github.com/bonniejools/cabot-alert-twilio/tarball/1.1.3'
      )

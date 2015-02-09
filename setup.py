6989fdbc44fa8ba361be88896b658950f493d85e
David Buxton <david@arachnys.com>
2015-02-09 19:30:49 +0000

    Fixed name of view

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(name='cabot-alert-twilio',
-      version='1.1.3',
+      version='1.1.4',
       description='A twilio alert plugin for Cabot by Arachnys',
       author='Arachnys',
       author_email='info@arachnys.org',
       url='http://cabotapp.com',
       packages=find_packages(),
-      download_url= 'https://github.com/bonniejools/cabot-alert-twilio/tarball/1.1.3'
+      download_url= 'https://github.com/bonniejools/cabot-alert-twilio/tarball/1.1.4'
      )

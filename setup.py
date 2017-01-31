1cc318f6a0487da541cfd652481dbdc3ebf26d43
Frank Hamand <frankhamand@gmail.com>
2017-01-31 14:08:37 +0000

    Add migrations for Django 1.7
    Since django 1.7, plugins now need to create their own migrations
    
    Bump version to 1.2

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(name='cabot-alert-twilio',
-      version='1.1.4',
+      version='1.2.0',
       description='A twilio alert plugin for Cabot by Arachnys',
       author='Arachnys',
       author_email='info@arachnys.org',
       url='http://cabotapp.com',
       packages=find_packages(),
-      download_url= 'https://github.com/bonniejools/cabot-alert-twilio/tarball/1.1.4'
+      download_url='https://github.com/cabotapp/cabot-alert-twilio/archive/1.2.0.zip'
      )

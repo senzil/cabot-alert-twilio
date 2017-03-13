bcd265f5acae4fd12c97237558e1e1b3c5cdcfd2
Frank Hamand <frankhamand@gmail.com>
2017-03-13 17:16:53 +0000

    Fix urls for django 1.10

--- cabot_alert_twilio/urls.py
+++ cabot_alert_twilio/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from .views import twiml_callback
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^result/(?P<service_id>\d+)/twiml_callback/', twiml_callback, name="twiml-callback"),
-)
+]

99f1af4139758a010398c1a3d8d38dca35b4a4d3
Frank Hamand <frankhamand@gmail.com>
2017-03-24 18:20:30 +0000

    1.3.1

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(name='cabot-alert-twilio',
-      version='1.3.0',
+      version='1.3.1',
       description='A twilio alert plugin for Cabot by Arachnys',
       author='Arachnys',
       author_email='info@arachnys.org',
       url='http://cabotapp.com',
       packages=find_packages(),
-      download_url='https://github.com/cabotapp/cabot-alert-twilio/archive/1.3.0.zip'
+      download_url='https://github.com/cabotapp/cabot-alert-twilio/archive/1.3.1.zip'
      )
